1d6b5956796b978a1f7cb1185b7b4edd879e9350
nicolasmendez001 <mnikolas001@hotmail.com>
2020-04-27 17:17:58 -0500

    add about

--- app-senorial/src/app/profile/profile.component.ts
+++ app-senorial/src/app/profile/profile.component.ts
@@ -10,14 +10,28 @@ import { Storage } from '@ionic/storage';
 export class ProfileComponent implements OnInit {
 
   user: ModelUser;
+  showDir: boolean;
 
   constructor(private storage: Storage) {
     this.user = new ModelUser();
-   }
+  }
 
   ngOnInit() {
+    this.showDir = false;
     this.storage.get('user').then((value) => {
       this.user = value;
     });
+  }
+
+  saveData() {
+
+  }
+
+  showMyDir() {
+    this.showDir = !this.showDir;
+  }
+
+  deleteDir(name: string) {
+    this.user.direccion = this.user.direccion.filter(item => item != name);
   }
 }
